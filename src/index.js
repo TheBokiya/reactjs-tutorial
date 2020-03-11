802f39e448c1a7724f753bdd57be3b54a3604417
Tim Bodyka Heng <hengbodyka@gmail.com>
2020-03-11 18:01:52 +0700

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 // import App from "./App"
-import App from "./module/todoListProject"
+import App from "./module/conditionalRendering"
 
 
 import './style.css'
