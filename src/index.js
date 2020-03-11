8b3f4b3ee4ecb8f89a27ebb8f2895e4ddf29ea86
Tim Bodyka Heng <hengbodyka@gmail.com>
2020-03-11 11:57:25 +0700

    handling events in react

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 // import App from "./App"
-import App from "./module/todoListProject"
+import App from "./module/handlingEventsInReact"
 
 
 import './style.css'
---
+++ src/module/handlingEventsInReact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+var handleClick = () => {
+    console.log("I was clicked")
+}
+
+
+function App() {
+    return (
+        <div>
+            <img onMouseOver={() => console.log("Hovered")} src="https://www.fillmurray.com/200/100" alt=""/>
+            <br/>
+            <br/>
+            <button onClick={handleClick}>Click me</button>
+        </div>
+    )
+}
+
+export default App
